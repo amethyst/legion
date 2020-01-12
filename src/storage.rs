3325b9f4a29fd035f83123a0285796e8a34f4676
Karl Bergström <karl.anton.bergstrom@gmail.com>
2020-01-12 01:38:56 +0100

    Fix clippy warnings

--- src/storage.rs
+++ src/storage.rs
@@ -692,6 +692,7 @@ impl ArchetypeData {
         self.tags.validate(self.chunk_sets.len());
     }
 
+    #[allow(clippy::too_many_arguments)]
     pub(crate) fn clone_merge<C: crate::world::CloneMergeImpl>(
         &mut self,
         src_world: &crate::world::World,
