9f37c5ab5855100e7051daa7abe2c7d9d5cbd0f7
Lukas Wirth <lukastw97@gmail.com>
2020-10-19 14:07:37 +0200

    Re-export FetchMut from systems

--- src/systems.rs
+++ src/systems.rs
@@ -3,7 +3,8 @@
 pub use crate::internals::systems::{
     command::{CommandBuffer, WorldWritable},
     resources::{
-        Fetch, Resource, ResourceSet, ResourceTypeId, Resources, SyncResources, UnsafeResources,
+        Fetch, FetchMut, Resource, ResourceSet, ResourceTypeId, Resources, SyncResources,
+        UnsafeResources,
     },
     schedule::{Builder, Executor, ParallelRunnable, Runnable, Schedule, Step},
     system::{QuerySet, System, SystemAccess, SystemBuilder, SystemFn, SystemId},
